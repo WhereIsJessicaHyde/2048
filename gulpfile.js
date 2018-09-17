cc191675aa0ef759147d3391134772f353eff2c9
Antonio Jesús Navarro Expósito <Antonio Jesus Navarro Exposito>
2018-09-17 19:07:51 +0200

    css funciona

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,26 @@ sass          = require('gulp-sass'),
 browserSync   = require('browser-sync'),
 reload        = browserSync.reload;
 
-gulp.task('browser-sync', function() {
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    //.pipe(reload({stream: true}));
+})
+
+gulp.task('server', function() {
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
+
+    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('index.html').on('change'.reload);
+    gulp.watch('css\style.css').on('change'.reload);
+    gulp.watch('js\dom.js').on('change'.reload);
+    gulp.watch('js\main.js').on('change'.reload);
+    gulp.watch('js\mov.js').on('change'.reload);
 });
 
-
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
-    .pipe(reload({stream: true}))
-});
-
-gulp.task('watch', ['browser-sync'], function () {
-  gulp.watch('./sass/**/*.scss', ['sass'])
-  gulp.watch('index.html').on('change'.reload)
-  gulp.watch('js\dom.js').on('change'.reload)
-  gulp.watch('js\main.js').on('change'.reload)
-  gulp.watch('js\mov.js').on('change'.reload)
-});
+gulp.task('default',['server']);
